9cb529f6189b5c05e99706a4b2f14bdaf7d7e015
gulzade <gulzadekrts@gmail.com>
2025-01-03 23:53:09 +0300

    uı design

--- Budget/ViewController.swift
+++ Budget/ViewController.swift
@@ -8,18 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var upgradeView: UIView!
+    
+    @IBOutlet weak var basicPlanView3: UIView!
+    @IBOutlet weak var basicPlanView2: UIView!
+    @IBOutlet weak var basicPlanView1: UIView!
+    @IBOutlet weak var cotentView: UIView!
+    @IBOutlet weak var upgradeLabel: UILabel!
+    @IBOutlet weak var imageview: UIImageView!
+    @IBOutlet weak var basicPlanLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-//        view.backgroundColor = UIColor
+        cotentView.layer.borderColor = UIColor.black.cgColor
+        cotentView.layer.borderWidth =  1
+        basicPlanView1.layer.cornerRadius = 16
+        basicPlanView1.layer.borderColor = UIColor.gray.cgColor
+        basicPlanView1.layer.borderWidth = 0.5
+        basicPlanView2.layer.cornerRadius = 16
+        basicPlanView3.layer.cornerRadius = 16
+        //        view.backgroundColor = UIColor
     }
 
 
-}
-
-#Preview() {
-    ViewController()
 }
 
